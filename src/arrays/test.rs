681b12950092e83e5b67d915dcc6ac5dd61d93d8
Morten Ruwald <smoggie70@hotmail.com>
2020-02-24 21:05:47 +0100

    Renamed Project File

---
+++ src/arrays/test.rs
@@ -0,0 +1,9 @@
+pub mod test;
+
+pub fn run() {
+     let x = "Morten";
+     let y = "Ruwald";
+
+    println!("Hello {}, {}", x, y)
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    //run();
+
+    test_one();
+    //arrays::test::run();
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+}
+
+// Public function
+pub fn test_one() {
+    println!("Moose!")
 }
